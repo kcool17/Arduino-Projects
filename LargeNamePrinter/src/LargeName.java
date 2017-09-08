14d0b22f16804b91413377a23a58b410c124bb5a
Kyle Sawicki <sawickik20@student.westboroughk12.org>
2017-09-08 13:56:32 -0400

    My hands are typing words

--- LargeNamePrinter/src/LargeName.java
+++ LargeNamePrinter/src/LargeName.java
@@ -1,9 +1,117 @@
 
 public class LargeName {
+	String a1=" *** ";
+	String a2="*   *";
+	String a3="*****";
+	String a4="*   *";
+	String a5="*   *";
+	
+	String b1="**** ";
+	String b2="*   *";
+	String b3="**** ";
+	String b4="*   *";
+	String b5="**** ";
+	
+	String c1=" *** ";
+	String c2="*   *";
+	String c3="*    ";
+	String c4="*   *";
+	String c5=" *** ";
+	
+	String d1="**** ";
+	String d2="*   *";
+	String d3="*   * ";
+	String d4="*   *";
+	String d5="**** ";
+	
+	String e1="*****";
+	String e2="*    ";
+	String e3="*****";
+	String e4="*    ";
+	String e5="*****";
+	
+	String f1="*****";
+	String f2="*    ";
+	String f3="*****";
+	String f4="*    ";
+	String f5="*    ";
+	
+	String g1=" *** ";
+	String g2="*    ";
+	String g3="*  **";
+	String g4="*   *";
+	String g5=" *** ";
+	
+	String h1="*   *";
+	String h2="*   *";
+	String h3="*****";
+	String h4="*   *";
+	String h5="*   *";
+	
+	String i1="*****";
+	String i2="  *  ";
+	String i3="  *  ";
+	String i4="  *  ";
+	String i5="*****";
 
+	String j1="*****";
+	String j2="  *  ";
+	String j3="  *  ";
+	String j4="* *  ";
+	String j5="***  ";
+	
+	String k1="*   *";
+	String k2="*  * ";
+	String k3="***";
+	String k4="*  *  ";
+	String k5="*   *";
+	
+	String l1="*    ";
+	String l2="*    ";
+	String l3="*    ";
+	String l4="*    ";
+	String l5="*****";
+	
+	
+	String m1="*   *";
+	String m2="** **";
+	String m3="* * *";
+	String m4="*   *";
+	String m5="*   * ";
+	
+	String n1="*   *";
+	String n2="**  *";
+	String n3="* * *";
+	String n4="*  **";
+	String n5="*   *";
+	
+	String o1=" *** ";
+	String o2="*   *";
+	String o3="*   * ";
+	String o4="*   *";
+	String o5=" *** ";
+	
+	String p1="**** ";
+	String p2="*   *";
+	String p3="**** ";
+	String p4="*    ";
+	String p5="*    ";
+	
+	String q1=" *** ";
+	String q2="*   *";
+	String q3="* ***";
+	String q4="* * *";
+	String q5=" *** ";
+	
+	String r1="*****";
+	String r2="*    ";
+	String r3="*****";
+	String r4="*    ";
+	String r5="*    ";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
 	}
 
 }
