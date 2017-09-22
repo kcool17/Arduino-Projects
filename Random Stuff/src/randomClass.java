e816eed262afdff9fbdb1adc2a1a80039e60d04a
Kyle Sawicki <sawickik20@student.westboroughk12.org>
2017-09-22 09:55:30 -0400

    Haaaaaaands

---
+++ Random Stuff/src/randomClass.java
@@ -0,0 +1,12 @@
+public class randomClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double b=4;
+		double a=4;
+		double c =1;
+		double x = (-b + Math.sqrt(Math.pow(b, 2)-(4*a*c)))/(2*a);
+		System.out.println(x);
+	}
+
+}
