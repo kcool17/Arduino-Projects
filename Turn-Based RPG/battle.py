4f3774fa9ac3c2a02c809463ca3f7c92c0876211
Kyle Sawicki <sawickik20@student.westboroughk12.org>
2017-10-04 14:27:06 -0400

    Added a few things to the battle menu

--- Turn-Based RPG/battle.py
+++ Turn-Based RPG/battle.py
@@ -2,12 +2,7 @@
 from player import Player
 from enemy import Enemy
 
-#Battle Menu Dictionary- This is what the player chooses from to attack, defend, use an item, etc.
-battle_menu = {
-    
 
-
-    }
 #Enemy Dictionary- Contains all of the info about the enemies in the game, to be input into the class
 #Format: "Enemy_Name: [healthPoints, attackPower]"
 enemy_dictionary = {
@@ -24,32 +19,49 @@ class Battle():
         self.player1 = player1
 
     def player1Turn(self):
-        try:
-            menuSelection = int(raw_input("What menu item?"))
-            if menuSelection == 1:
-                self.enemy1.getDamaged(self.player1.attackEnemy())
-                print "Player 1 does things!"
-        except:
-            print "Try again!"
+        exitLoop = False
+        while exitLoop == False:
+            try:
+                menuSelection = int(raw_input("What menu item?"))
+                if menuSelection == 1:
+                    self.enemy1.getDamaged(self.player1.attackEnemy())
+                    print "Player 1 does things!"
+                    exitLoop = True
+                else:
+                    print "Not a Vaild Selection, Try again!"
+            except:
+                print "Not a Valid Selection, Try again!"
         
 
     def enemy1Turn(self):
         self.player1.getDamaged(self.enemy1.attackPlayer())
         print "Enemy 1 does things!"
-        
+
+    def printMenu(self, playerTurn):
+        #Battle Menu Dictionary- This is what the player chooses from to attack, defend, use an item, etc.
+        battle_menu = {
+        "player1HP":"Player 1 HP: "+str(self.player1.hitPoints),
+        "enemy1HP":"Enemy 1 HP: "+str(self.enemy1.hitPoints),
+
+
+        }
+        #Prints Battle Menu
+        for item in battle_menu:
+            print battle_menu[item]
+
+     
     def doBattle(self):
         exitLoop = False
         winner = 0
         turnNumber = 1
         while exitLoop != True:
-            #Displays HP, etc.
-            print self.player1.hitPoints
-            print self.enemy1.hitPoints
             #Takes a turn
             if turnNumber == 1:
+                self.printMenu(True)
                 self.player1Turn()
                 turnNumber= turnNumber + 1
             elif turnNumber == 2:
+                self.printMenu(False)
                 self.enemy1Turn()
                 turnNumber = 1
                 
--- Turn-Based RPG/main.py
+++ Turn-Based RPG/main.py
@@ -13,3 +13,4 @@ myBattle.doBattle()
 
 
 
+
