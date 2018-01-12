cd33efdc825ab6d464f68a351e73a9bc95a7716b
Kyle Sawicki <sawickik20@student.westboroughk12.org>
2018-01-12 08:16:27 -0500

    Mistaaaaaaaaaaaaaakes.

--- Dictionary Project/src/Dictionary_2_0.java
+++ Dictionary Project/src/Dictionary_2_0.java
@@ -16,7 +16,7 @@ public class Dictionary_2_0 {
 		while(in.hasNext() && elementNum<200)
 	    {
 	    	currentWord++;
-	    	if (currentWord%1000==0) {
+	    	if ((currentWord+1)%1000==0) {
 	    		stuff[elementNum] = in.next();
 	    		elementNum++;
 	    	}else {
@@ -54,17 +54,29 @@ public class Dictionary_2_0 {
 		return vowels;
 	}
 	
-	public static String secondShort(String[] arr, String shortest) {
-		String currentShort="";
-		int shortLength=1000;
-		int oldMin = shortest.length();
+	
+	public static String findLarge(String[] arr) {
+		String currentLarge = arr[0];
+		int largeLength = currentLarge.length();
 		for(int x=0; x<arr.length; x++) {
-			if(arr[x].length()<=shortLength && arr[x].length()>oldMin) {
-				currentShort = arr[x];
-				shortLength = arr[x].length();
+			if(arr[x].length()>largeLength) {
+				currentLarge=arr[x];
+				largeLength=arr[x].length();
 			}
 		  }
-		return currentShort;
+		return currentLarge;
+	}
+	public static String secondLarge(String[] arr, String largest) {
+		String currentLarge="";
+		int largeLength=0;
+		int oldMax = largest.length();
+		for(int x=0; x<arr.length; x++) {
+			if(arr[x].length()>=largeLength && arr[x].length()<oldMax) {
+				currentLarge = arr[x];
+				largeLength = arr[x].length();
+			}
+		  }
+		return currentLarge;
 	}
 	
 	public static String[] sixOrMore(String[] arr) {
@@ -95,9 +107,10 @@ public class Dictionary_2_0 {
 	    System.out.println();
 	    System.out.println();
 	    String shortest =findSmall(newDictionary);
+	    String large = findLarge(newDictionary); //Finds the largest word in the dictionary, for later use.
 	    System.out.println("Smallest Word: " +shortest); //Finds the smallest word in the array, and prints it
 	    System.out.println("Smallest Word's Number of Vowels: " +numVowels(shortest)); //Finds the number of vowels in the smallest word in the array, and prints it
-	    System.out.println("Second Smallest Word: "+ secondShort(newDictionary, shortest));
+	    System.out.println("Second Largest Word: "+ secondLarge(newDictionary, large)); //Finds the second largest wor din the dictionary.
 	    String[] sixVowel = sixOrMore(newDictionary);//Finds all the word with 6 or more vowels, and adds them to an array
 	    System.out.println();
 	    System.out.println("Words With 6 or More Vowels: ");
