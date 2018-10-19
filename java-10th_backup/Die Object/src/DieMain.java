58b9e415071ce4fe565661c26990ea5a4cae4da6
Kyle Sawicki <kcool3000@gmail.com>
2018-10-18 21:32:05 -0400

    Die.

--- java-10th_backup/Die Object/src/DieMain.java
+++ java-10th_backup/Die Object/src/DieMain.java
@@ -4,11 +4,7 @@ public class DieMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Creates 5 new die objects
-		Die die1 = new Die();
-		Die die2 = new Die();
-		Die die3 = new Die();
-		Die die4 = new Die();
-		Die die5 = new Die();
+		Die die1 = new Die(), die2 = new Die(), die3 = new Die(), die4 = new Die(), die5 = new Die();
 		Die[] dieArr = {die1, die2, die3, die4, die5};
 		int[] rollArr = new int[5];
 		//Variables for determining how many moves until doubles are rolled.
