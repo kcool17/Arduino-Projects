1a0c2348c15d2c871fba586f9643c271d8e90a98
Kyle Sawicki <sawickik20@student.westboroughk12.org>
2018-12-04 10:57:39 -0500

    Hierarchy Project Beginning

--- java-10th_backup/Inheritance/src/Textbook.java
+++ java-10th_backup/Inheritance/src/Textbook.java
@@ -1,4 +1,12 @@
 
-public class Textbook {
-
+public class Textbook extends Book{
+	private int indexPages;
+	public Textbook(int pages, int indexPages) {
+		super(pages);
+		this.indexPages = indexPages;
+	}
+	
+	public int getPages() {
+		return this.getPages() + indexPages;
+	}
 }
