9f57d984cee3b09baba632f409da1e10fd0c27fd
Kyle Sawicki <kcool3000@gmail.com>
2018-12-13 19:28:26 -0500

    Commit to show Nick how commits work
    Don't worry about this.

---
+++ java-10th_backup/Remedial Class for the Disabled/src/MainRunner.java
@@ -0,0 +1,10 @@
+
+public class MainRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Students are late, again.");
+		System.out.println("Join VC, nerd.");
+		System.out.println("In Discord.");
+	}
+
+}
