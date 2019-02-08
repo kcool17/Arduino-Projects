db83ee47294983fd5f333f1067963bb327134a17
Kyle Sawicki <sawickik20@student.westboroughk12.org>
2019-02-08 08:42:05 -0500

    ArrayLists

--- java-10th_backup/ArrayList Practice/src/ZooClient.java
+++ java-10th_backup/ArrayList Practice/src/ZooClient.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class ZooClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Animal> zoo = new ArrayList<Animal>();
+		zoo.add(new Dog("Snoopy"));
+		zoo.add(new Cat("Garfield"));
+		//System.out.println(zoo.get(1).getSpecies());
+		//for(Animal myAnimal : zoo) {
+		//	System.out.println(myAnimal.getSpecies());
+		//}
+		ListIterator<Animal> myIter= zoo.listIterator();
+		while (myIter.hasNext()) {
+			System.out.println(myIter.next().getSpecies());
+		}
 	}
 
 }
